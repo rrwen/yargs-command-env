3ab718d7625bab5191b0079b914e1f17427bdef9
rwenite <rwenite@gmail.com>
2018-01-07 22:36:50 -0500

    Passed tests

--- index.js
+++ index.js
@@ -1,16 +1,217 @@
 // Richard Wen
 // rrwen.dev@gmail.com
 
+var envfile = require('envfile');
+var fs = require('fs');
+
 /**
- * Description.
+ * Yargs command for managing env files.
  *
- * @module yargs-command-env
- * @param {Object} [options={}] parameter description.
- * @returns {Object} return description.
+ * * {@link https://www.npmjs.com/package/yargs Yargs npm Package}
+ * * {@link https://www.env.org/ JavaScript Object Notation (JSON)}
+ *
+ * @module env
+ * @param {Object} [options={}] options for this function.
+ * @param {string} options.file default path to env file for yargs.
+ *
+ * * The command line argument `[--env]` will take priority over `options.file` 
+ *
+ * @param {string} [options.command='env'] name of base `<env>` command.
+ *
+ * 1. **Original**: `env <task> [key] [value] [--env]`
+ * 2. **options.command='newenv':** `newenv <task> [key] [value] [--env]`
+ *
+ * @param {Object} [options.defaults={}] default env object to be used.
+ *
+ *  * If `options.defaults` is undefined, the object `argv.env` will be used before defaulting to `{}` 
+ *
+ * @param {string} [options.describe='describe'] description for base `<env>` command.
+ * @param {Object} [options.task={}] options for `<task>` commands.
+ * @param {Object} [options.task.command='task'] name of `<task>` command.
+ *
+ * 1. **Original**: `env <task> [key] [value] [--env]`
+ * 2. **options.task.command='newtask':** `env <newtask> [key] [value] [--env]`
+ *
+ * @param {string} [options.task.key='key'] name of optional `[key]` argument.
+ *
+ * 1. **Original**: `env <task> [key] [value] [--env]`
+ * 2. **options.task.key='newkey':** `env <task> [newkey] [value] [--env]`
+ *
+ * @param {string} [options.task.value='value'] name of optional `[value]` argument.
+ *
+ * 1. **Original**: `env <task> [key] [value] [--env]`
+ * 2. **options.task.value='newvalue':** `env <task> [key] [newvalue] [--env]`
+ *
+ * @param {string} [options.task.env='env'] name of optional `[--env]` argument.
+ *
+ * 1. **Original**: `env <task> [key] [value] [--env]`
+ * 2. **options.task.env='newenv':** `env <task> [key] [value] [--newenv]`
+ *
+ * @param {Object} [options.task.reset='reset'] name of `<task`> command for `reset`.
+ *
+ * 1. **Original**: `env reset [--env]`
+ * 2. **options.task.reset='newreset':** `env newreset [--env]`
+ *
+ * @param {Object} [options.task.clear='clear'] name of `<task`> command for `clear`.
+ *
+ * 1. **Original**: `env clear [--env]`
+ * 2. **options.task.reset='newclear':** `env newclear [--env]`
+ *
+ * @param {Object} [options.task.view='view'] name of `<task`> command for `view`.
+ *
+ * 1. **Original**: `env view [--env]`
+ * 2. **options.task.view='newview':** `env newview [--env]`
+ *
+ * @param {Object} [options.task.delete='delete'] name of `<task`> command for `delete`.
+ *
+ * 1. **Original**: `env delete [--env]`
+ * 2. **options.task.delete='newdelete':** `env newdelete [key] [--env]`
+ *
+ * @param {Object} [options.task.set='set'] name of `<task`> command for `set`.
+ *
+ * 1. **Original**: `env set [--env]`
+ * 2. **options.task.set='newset':** `env newset [key] [value] [--env]`
+ *
+ * @returns {Object} Yargs {@link https://github.com/yargs/yargs/blob/master/docs/advanced.md#providing-a-command-module Command Module} with the following properties (`out` is the returned Object):
+ *
+ * * `out.command`: the command string in the form of `options.command <options.task.command> [options.task.key] [options.task.value] [--options.task.env]`
+ * * `out.describe`: the description string for `out.command`
+ * * `out.handler`: the function that manages the env file and returns an `argv` Object containing command line arguments
  *
  * @example
- * var yargscommandenv = require('yargs-command-env');
+ * // *** DEFAULT ***
+ *
+ * var yargs = require('yargs');
+ *
+ * // (env) Load command with path to env file
+ * var env = require('yargs-command-env')({file: './path/to/env.env'});
+ *
+ * // (yargs) Add command to manage env file
+ * var argv = yargs.command(env).argv;
+ *
+ * // *** CUSTOM ***
+ *
+ * var yargs = require('yargs');
+ *
+ * // (options_command) Setup command options
+ * options = {};
+ * options.command = 'env2';
+ * options.defaults = {field: 'value'};
+ * options.describe = 'Description';
+ *
+ * // (options_task) Setup task options
+ * options.task = {};
+ * options.task.command = 'task2';
+ * options.task.key = 'key2';
+ * options.task.value = 'value2';
+ * options.task.env = 'env2';
+ * options.task.reset = 'reset2';
+ * options.task.clear = 'clear2';
+ * options.task.view = 'view2';
+ * options.task.delete = 'delete2';
+ * options.task.set = 'set2';
+ *
+ * // (env) Load command with options
+ * var env = require('yargs-command-env')(options);
+ *
+ * // (yargs) Add command to manage env file
+ * var argv = yargs.command(env).argv;
+ *
  */
 module.exports = options => {
 	options = options || {};
+	
+	// (default_task) Default task options
+	options.task = options.task || {};
+	options.task.command = options.task.command || 'task' ;
+	options.task.key = options.task.key || 'key';
+	options.task.value = options.task.value || 'value';
+	options.task.env = options.task.env || 'env';
+	options.task.reset = options.task.reset || 'reset';
+	options.task.clear = options.task.clear || 'clear';
+	options.task.view = options.task.view || 'view';
+	options.task.delete = options.task.delete || 'delete';
+	options.task.set = options.task.set || 'set';
+	
+	// (default) Default options
+	options.command = options.command || 'env';
+	options.describe = options.describe || 
+		'manage default env' +
+		'\n\n<' + options.task.command + '> is one of:' +
+		'\n\n* ' + options.task.set +
+		'\n* ' + options.task.delete +
+		'\n* ' + options.task.view +
+		'\n* ' + options.task.clear +
+		'\n* ' + options.task.reset +
+		'\n\nSet option to value' +
+		'\n> ' +  options.task.set + ' [' + options.task.key + '] [' + options.task.value + ']' +
+		'\n\nRemove default option' +
+		'\n> ' + options.task.delete + ' [' + options.task.key + ']' +
+		'\n\nView default options' +
+		'\n> ' + options.task.view +
+		'\n\nClear default options' +
+		'\n> ' + options.task.clear +
+		'\n\nReset default options' +
+		'\n> ' + options.task.reset +
+		'\n\nManage other env file' +
+		'\n> ' +  options.task.set + ' [' + options.task.key + '] [' + options.task.value + ']' + ' --' + options.task.env + ' other.env' +
+		'\n> ' + options.task.delete + ' [' + options.task.key + ']' +  ' --' + options.task.env + ' other.env' +
+		'\n> ' + options.task.view + ' --' + options.task.env + ' other.env' +
+		'\n> ' + options.task.clear + ' --' + options.task.env + ' other.env' +
+		'\n> ' + options.task.reset + ' --' + options.task.env + ' other.env';
+		
+	// (command_module) Create yargs command module
+	var out = {};
+	out.command = options.command + ' <' + options.task.command + '> [' + options.task.key + '] [' + options.task.value + '] [--' + options.task.env +  ']';
+	out.describe = options.describe;
+	out.handler = function(argv) {
+		var task = argv[options.task.command];
+		var file = argv[options.task.env] || options.file;
+		var defaults =  options.defaults || {};
+		
+		// (env_read) Read env file or create if not exists
+		if (!fs.existsSync(file)) {
+			fs.writeFileSync(file, envfile.stringifySync(defaults));
+		}
+		var env = envfile.parseFileSync(file);
+		
+		// (env_reset) Reset env file to defaults
+		if (task == options.task.reset) {
+			env = defaults;
+			fs.writeFileSync(file, envfile.stringifySync(env));
+			console.log('Reset defaults');
+		}
+		
+		// (env_clear) Clear env file
+		if (task == options.task.clear) {
+			env = {};
+			fs.writeFileSync(file, envfile.stringifySync(env));
+			console.log('Empty defaults');
+		}
+		
+		// (env_view) View defaults
+		if (task == options.task.view) {
+			console.log(envfile.stringifySync(env, null, 2));
+		}
+		
+		// (env_delete) Delete key from env file
+		if (task == options.task.delete) {
+			delete env[argv[options.task.key]];
+			delete process.env[argv[options.task.key]];
+			fs.writeFileSync(file, envfile.stringifySync(env));
+			console.log('Delete default', argv[options.task.key]);
+		}
+		
+		// (env_set) Set key to value for env file
+		if (task == options.task.set) {
+			env[argv[options.task.key]] = argv[options.task.value];
+			fs.writeFileSync(file, envfile.stringifySync(env));
+			console.log('Set default', argv[options.task.key], 'to', argv[options.task.value]);
+		}
+		
+		// (argv_return) Update argv with env
+		Object.assign(process.env, env);
+		return argv;
+	};
+	return(out);
 };
--- tests/test.js
+++ tests/test.js
@@ -4,8 +4,8 @@
 // (packages) Package dependencies
 var fs = require('fs');
 var moment = require('moment');
-var yargscommandenv = require('../index.js');
 var test = require('tape');
+var yargs = require('yargs');
 
 // (test_info) Get package metadata
 var json = JSON.parse(fs.readFileSync('package.json', 'utf8'));
@@ -22,11 +22,15 @@ for (var k in json.devDependencies) {
 if (!fs.existsSync('./tests/log')){
 	fs.mkdirSync('./tests/log');
 }
+if (!fs.existsSync('./tests/out')){
+	fs.mkdirSync('./tests/out');
+}
 var testFile = './tests/log/test_' + json.version.split('.').join('_') + '.txt';
 test.createStream().pipe(fs.createWriteStream(testFile));
 test.createStream().pipe(process.stdout);
 
 // (test) Run tests
+console.log = function(){};
 test('Tests for ' + json.name + ' (' + json.version + ')', t => {
 	t.comment('Node.js (' + process.version + ')');
 	t.comment('Description: ' + json.description);
@@ -34,21 +38,103 @@ test('Tests for ' + json.name + ' (' + json.version + ')', t => {
 	t.comment('Dependencies: ' + testedPackages.join(', '));
 	t.comment('Developer: ' + devPackages.join(', '));
 
-	// (test_pass) Pass a test
-	t.pass('(MAIN) test pass');
-
-	// (test_equal) Equal test
-	var actual = 1;
-	var expected = 1;
-	t.equal(actual, expected, '(A) Equal test');
-
-	// (test_deepequal) Deep equal test
-	var actual = {a: 1, b: {c: 2}, d: [3]};
-	var expected = {a: 1, b: {c: 2}, d: [3]};
-	t.deepEquals(actual, expected, '(B) Deep equal test');
-
-	// (test_fail) Fail a test
-	// t.fail('(MAIN) test fail');
-
+	// (test_variables) Test variables used for all tests
+	var actual, expected;
+	
+	// (test_default) Tests on default options
+	t.comment('(A) tests on default options');
+	var env = require('../index.js');
+	var defaultHandler = env().handler;
+	
+	// (test_default_clear) Test default clear command
+	actual = defaultHandler({_: ['env'], task: 'clear', env: './tests/out/env.env', key: {option: 'value'}});
+	expected = {_: ['env'], task: 'clear', env: './tests/out/env.env', key: {option: 'value'}};
+	t.deepEquals(actual, expected, '(A) env clear');
+	
+	// (test_default_reset) Test default reset command
+	actual = defaultHandler({_: ['env'], task: 'reset', env: './tests/out/env.env'});
+	expected = {_: ['env'], task: 'reset', env: './tests/out/env.env'};
+	t.deepEquals(actual, expected, '(A) env reset');
+	
+	// (test_default_view) Test default view command
+	actual = defaultHandler({_: ['env'], env: './tests/out/env.env', task: 'view'});
+	expected = {_: ['env'], env: './tests/out/env.env', task: 'view'};
+	t.deepEquals(actual, expected, '(A) env view');
+	
+	// (test_default_set) Test default set command
+	actual = defaultHandler({_: ['env'], task: 'set', env: './tests/out/env.env', key: 'field', value: 'value'});
+	expected = {_: ['env'], task: 'set', env: './tests/out/env.env', key: 'field', value: 'value'};
+	t.deepEquals(actual, expected, '(A) env set');
+	
+	// (test_default_set_env) Test default set command in process.env
+	actual = process.env.field;
+	expected = 'value';
+	t.equals(actual, expected, '(A) process.env set')
+	
+	// (test_default_delete) Test default delete command
+	actual = defaultHandler({_: ['env'], task: 'delete', env: './tests/out/env.env', key: 'field'});
+	expected = {_: ['env'], task: 'delete', env: './tests/out/env.env', key: 'field'};
+	t.deepEquals(actual, expected, '(A) env delete');
+	
+	// (test_default_delete_env) Test default delete command in process.env
+	actual = process.env.field;
+	expected = undefined;
+	t.equals(actual, expected, '(A) process.env delete')
+	
+	// (test_custom) Tests on custom options
+	t.comment('(B) tests on custom options');
+	env = require('../index.js')({
+		file: './tests/out/env2.env',
+		command: 'env2',
+		defaults: {field: 'value'},
+		describe: 'Description',
+		task: {
+			command: 'task2',
+			key: 'key2',
+			value: 'value2',
+			env: 'env2',
+			reset: 'reset2',
+			clear: 'clear2',
+			view: 'view2',
+			delete: 'delete2',
+			set: 'set2'
+		}
+	});
+	var customHandler = env.handler;
+	
+	// (test_custom_clear) Test custom clear command
+	actual = customHandler({_: ['env2'], task2: 'clear2', env: {option: 'value'}});
+	expected = {_: ['env2'], task2: 'clear2', env: {option: 'value'}};
+	t.deepEquals(actual, expected, '(B) env2 clear2');
+	
+	// (test_custom_reset) Test custom reset command
+	actual = customHandler({_: ['env2'], task2: 'reset2'});
+	expected = {_: ['env2'], task2: 'reset2'};
+	t.deepEquals(actual, expected, '(B) env2 reset2');
+	
+	// (test_custom_view) Test custom view command
+	actual = customHandler({_: ['env2'], env2: './tests/out/env2.env', task2: 'view2'});
+	expected = {_: ['env2'], env2: './tests/out/env2.env', task2: 'view2'};
+	t.deepEquals(actual, expected, '(B) env2 view2');
+	
+	// (test_custom_set) Test custom set command
+	actual = customHandler({_: ['env2'], task2: 'set2', key2: 'field', value2: 'value2'});
+	expected = {_: ['env2'], task2: 'set2', key2: 'field', value2: 'value2'};
+	t.deepEquals(actual, expected, '(B) env2 set2');
+	
+	// (test_custom_set_env) Test custom set command in process.env
+	var setSecond = customHandler({_: ['env2'], task2: 'set2', key2: 'field2', value2: 'value3'});
+	actual = {field: process.env.field, field2: process.env.field2};
+	expected = {field: 'value2', field2: 'value3'};
+	t.deepEquals(actual, expected, '(A) process.env set2')
+	
+	// (test_custom_delete) Test custom delete command
+	actual = customHandler({_: ['env2'], task2: 'delete2', key2: 'field'});
+	expected = {_: ['env2'], task2: 'delete2', key2: 'field'};
+	t.deepEquals(actual, expected, '(B) env2 delete2');
+	
+	// (test_end) End tests and cleanup files
+	fs.unlinkSync('./tests/out/env.env');
+	fs.unlinkSync('./tests/out/env2.env');
 	t.end();
 });
